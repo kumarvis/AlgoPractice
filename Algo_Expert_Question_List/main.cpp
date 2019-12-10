6038374325bad99fe8c35ec885f7810b6cb80bb2
Kumar Vishal <shunya@pop-os.localdomain>
2019-12-10 09:23:10 +0530

    development

---
+++ Algo_Expert_Question_List/main.cpp
@@ -0,0 +1,7 @@
+#include "two_number_sum.hpp"
+
+
+int main(){
+    run_two_number_sum();
+    return 0 ;
+}
---
+++ Algo_Expert_Question_List/two_number_sum.hpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std ;
+
+
+void run_two_number_sum(){
+    int N = 2 ;
+    cout<<"print1"<<endl;
+    cout<<"print2"<<endl;
+}
