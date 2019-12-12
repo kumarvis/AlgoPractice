00acb955d51b4e7dc68fff3b6c40d9826e74b8d1
Kumar Vishal <shunya@pop-os.localdomain>
2019-12-12 17:58:37 +0530

    two number sum

--- Algo_Expert_Question_List/two_number_sum.hpp
+++ Algo_Expert_Question_List/two_number_sum.hpp
@@ -1,9 +1,16 @@
 #include<iostream>
+#include<vector>
 using namespace std ;
 
+void fun_two_number_sum(vector<int> &vec_arr, int Sum){
+    
+}
 
 void run_two_number_sum(){
-    int N = 2 ;
-    cout<<"print1"<<endl;
-    cout<<"print2"<<endl;
+    int arr[] = {3, 5, -4, 8, 11, 1, -1, 6};
+    int N = sizeof(arr)/ sizeof(arr[0]);
+    int S = 10 ;
+    vector<int> vec_arr(arr, arr+N) ;
+    fun_two_number_sum(vec_arr, S);
+    
 }
