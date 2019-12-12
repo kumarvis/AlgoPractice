d1ccee231a8d4163bf67d1ea62d1e27886121d8a
Kumar Vishal <shunya@pop-os.localdomain>
2019-12-12 18:28:43 +0530

    two number sum

--- Algo_Expert_Question_List/two_number_sum.hpp
+++ Algo_Expert_Question_List/two_number_sum.hpp
@@ -1,9 +1,20 @@
 #include<iostream>
 #include<vector>
+#include<unordered_set>
+
 using namespace std ;
 
 void fun_two_number_sum(vector<int> &vec_arr, int Sum){
-    
+    unordered_set<int> my_set ;
+    for(int ii=0 ; ii < vec_arr.size() ; ii++){
+        int tmp = Sum - vec_arr[ii];
+        if(my_set.find(tmp) == my_set.end()){
+            my_set.insert(vec_arr[ii]);
+        }else{
+            cout<<"Find the match"<<endl;
+            cout<<"Pair"<<vec_arr[ii]<<" "<<tmp<<endl;
+        }
+    }
 }
 
 void run_two_number_sum(){
