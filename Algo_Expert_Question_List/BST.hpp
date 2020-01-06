a1f2d4505acf3dc14cc96965d58785307858e7ee
Kumar Vishal <shunya@pop-os.localdomain>
2020-01-06 16:15:19 +0530

    changes saved

--- Algo_Expert_Question_List/BST.hpp
+++ Algo_Expert_Question_List/BST.hpp
@@ -11,14 +11,25 @@ struct BST_Node{
     BST_Node *right ;
 };
 
-void insert_node(BST_Node *&BST, int val){
+void insert_node(BST_Node *&tree, int val){
+    if(tree==NULL){
+        BST_Node *tmp = new BST_Node();
+        tmp->value = val ;
+        tmp->left = NULL ;
+        tmp->right = NULL ;
+    }else{
+        if(tree->value > val){
+            
+        }else{
 
+        }
+    }
 }
 
 BST_Node* create_bst_from_vector(vector<int> vec_arr){
     BST_Node *bst = NULL ;
     for(int ii=0 ; ii < vec_arr.size(); ii++){
-        
+        insert_node(bst, vec_arr[ii]);
     }
     return bst ;
 }
