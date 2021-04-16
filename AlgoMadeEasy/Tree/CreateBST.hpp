eb39b38fffa2b298882d12bd0d2a3d0dcd8654d5
Kumar Vishal <kumarvis@gmail.com>
2021-04-17 00:23:29 +0530

    Tree Practive from book DS and Algo Made Easy

---
+++ AlgoMadeEasy/Tree/CreateBST.hpp
@@ -0,0 +1,52 @@
+#ifndef CREATE_BST_HH
+#define CREATE_BST_HH
+
+#include<iostream>
+#include <vector>
+
+using namespace std ;
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+void printInorder(struct Node *root){
+    if(root){
+        printInorder(root->left);
+        cout<<root->data<<"\t";
+        printInorder(root->right);
+    }
+}
+
+struct Node* insertNode(struct Node *root, int data){
+    if(root ==  NULL){
+        struct Node *tmp = new Node();
+        tmp->data = data;
+        tmp->left = NULL ;
+        tmp->right = NULL ; 
+        return tmp ;
+    }else {
+        if(data < root->data){
+            root->left = insertNode(root->left, data);
+        }else{
+            root->right = insertNode(root->right, data);
+        }   
+    }
+    return root ;
+}
+
+void run_createBST(){
+    vector<int> vec_arr = {30, 20, 40, 15, 25, 35, 45};
+    struct Node *BST = NULL ;
+    for (int item : vec_arr){
+        BST = insertNode(BST, item);
+    }
+    cout<<"Print Inorder for Check"<<endl;
+    printInorder(BST);
+    cout<<endl;
+
+}
+
+#endif //CREATE_BST_HH
---
+++ AlgoMadeEasy/Tree/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "CreateBST.hpp"
+
+using namespace std ;
+
+int main(){
+    run_createBST();
+}
