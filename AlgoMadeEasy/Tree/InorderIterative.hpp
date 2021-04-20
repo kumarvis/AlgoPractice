646e58dcba3f2284c1bd38bba697e427435a88b7
Kumar Vishal <kumarvis@gmail.com>
2021-04-20 09:48:15 +0530

    InOrderIterative working need to test more

--- AlgoMadeEasy/Tree/InorderIterative.hpp
+++ AlgoMadeEasy/Tree/InorderIterative.hpp
@@ -12,19 +12,35 @@ void inorderIterative(struct Node *root) {
 	struct Node* tmp = root;
 	
 	do {
-		node_stk.push(tmp);
-		tmp = tmp->left;
-	} while (tmp);
-			
-	struct Node* curr = node_stk.top();
-	cout << curr->data << "\t";
-	tmp = curr->right;
-	node_stk.pop();
 
+		while (tmp) {
+			node_stk.push(tmp);
+			tmp = tmp->left;
+		}
+
+		struct Node* curr = node_stk.top();
+		cout << curr->data << "\t";
+		node_stk.pop();
+
+		if (curr->right != NULL) {
+			tmp = curr->right;
+			node_stk.push(tmp);
+			tmp = tmp->left;
+		}
+		else {
+			tmp = NULL;
+		}
+		
+
+	} while (!node_stk.empty());		
 }
+	
+	
+
+
 
 void run_inorderIterative() {
-	vector<int> vec_arr = { 35, 20, 40, 15, 25, 35, 45 };
+	vector<int> vec_arr = { 30, 20, 40, 15, 25, 35, 45 };
 	struct Node* root = NULL;
 	for (auto& it : vec_arr) {
 		root = insertNode(root, it);
