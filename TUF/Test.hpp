30b4b72d5574e206e8e411a90f732dec4a90cfc2
Kumar Vishal <kumarvis@gmail.com>
2024-09-22 23:01:53 +0530

    Starting TUF Exercise

---
+++ TUF/Test.hpp
@@ -0,0 +1,12 @@
+#ifndef TUF_TEST_HPP
+#define TUF_TEST_HPP
+
+#include "common_header.hh"
+
+using namespace std ; 
+
+void test_tuf(){
+    cout<<"Test TUF"<<endl;
+}
+
+#endif  //
---
+++ TUF/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "Test.hpp"
+
+using namespace std ; 
+
+int main(){
+    test_tuf();
+    cout<<"break"<<endl;
+}
